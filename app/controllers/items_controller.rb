d24079a7bcbe4418a9a77a56f15213b50fc3c80a
TaiseiKunieda <taisei8271@icloud.com>
2021-02-05 23:31:45 +0900

    商品情報編集機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :contributor_confirmation, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -24,17 +25,26 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
+    else
+      render :edit
     end
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :state_id, :postage_id, :area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to  new_user_session_path unless user_signed_in?
   end
 end
