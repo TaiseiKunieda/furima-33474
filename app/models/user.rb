e338dda16d2f21499e3b138d14a4597b731638f4
TaiseiKunieda <taisei8271@icloud.com>
2021-02-05 17:31:39 +0900

    商品詳細表示機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :items
-  has_many :purchase
+  has_many :purchases
   
   with_options presence: true do
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
