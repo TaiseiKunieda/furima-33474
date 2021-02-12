bb85d3e87a92932b3eb20548947f88c25c07ccdd
TaiseiKunieda <taisei8271@icloud.com>
2021-02-12 14:22:37 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name_J, :last_name_J, :first_name_K, :last_name_K, :birthday])
   end
+
 end
